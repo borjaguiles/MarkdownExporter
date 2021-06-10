3c8b7000faedc765fe592aa7526c5a0db9ab411d
borjaguiles <borjaguiles@hotmail.com>
2021-06-11 01:47:22 +0200

    test: transform markdown section into html title

---
+++ MarkdownExporter/MarkdownExporterTests/HtmlMarkdownExporterShould.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using MdExport.Exporter;
+using MdExport.ExportTypeSelector;
+using Xunit;
+
+namespace MdExportTests
+{
+    public class HtmlMarkdownExporterShould
+    {
+        private MarkdownExporter _markdownExporter;
+        private HtmlExportSelector _selector;
+
+        public HtmlMarkdownExporterShould()
+        {
+            _markdownExporter = new MarkdownExporter();
+            _selector = new HtmlExportSelector();
+        }
+
+        [Fact]
+        public void TransformMarkdownsSectionIntoHtmlTitle()
+        {
+            var text = "# This is a title";
+            var resultText = _markdownExporter.Export(_selector, text);
+            var expectedTest = "<h1> this is a title </h1>";
+            expectedTest.Should().BeEquivalentTo(resultText);
+        }
+    }
+}
