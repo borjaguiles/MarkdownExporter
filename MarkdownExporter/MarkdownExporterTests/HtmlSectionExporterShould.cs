2adb548ec10659ee5385917066323dc103ee2de2
borjaguiles <borjaguiles@hotmail.com>
2021-06-11 01:58:43 +0200

    test: return html title given markdown first level section

---
+++ MarkdownExporter/MarkdownExporterTests/HtmlSectionExporterShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using MdExport.Exporter;
+using Xunit;
+
+namespace MdExportTests
+{
+    public class HtmlSectionExporterShould
+    {
+        private SectionExporter _sectionExporter;
+
+        [Fact]
+        public void ReturnHtmlTitleGivenMarkdownFirstLevelSection()
+        {
+            var markdownText = "# Section";
+            var resultText = _sectionExporter.ExportHtml(markdownText);
+            var expectedText = "<h1> Section</h1>";
+            expectedText.Should().BeEquivalentTo(resultText);
+        }
+    }
+}
