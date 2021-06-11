90777a88f4923b58f347531b273f146be4a88720
borjaguiles <borjaguiles@hotmail.com>
2021-06-11 03:21:42 +0200

    test: add more bold export tests

--- MarkdownExporter/MarkdownExporterTests/BoldExporterShould.cs
+++ MarkdownExporter/MarkdownExporterTests/BoldExporterShould.cs
@@ -18,6 +18,7 @@ namespace MdExportTests
         [Theory]
         [InlineData("**This is bold**", "<b>This is bold</b>")]
         [InlineData("# Section/r/n**This is bold**/r/n**This is also bold**", "# Section/r/n<b>This is bold</b>/r/n<b>This is also bold</b>")]
+        [InlineData("**This is bold****This is also bold**", "<b>This is bold</b><b>This is also bold</b>")]
         public void TransformMarkdownBoldIntoHtmlBold(string testText, string expectedText)
         {
             var resultText = _boldExporter.ExportHtml(testText);
