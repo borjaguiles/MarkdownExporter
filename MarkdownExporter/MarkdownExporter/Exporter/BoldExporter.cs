7b219347a15f99e2a47e4b42192f7362b9d96c8f
borjaguiles <borjaguiles@hotmail.com>
2021-06-11 02:56:36 +0200

    test: transform markdown bold into html bold

---
+++ MarkdownExporter/MarkdownExporter/Exporter/BoldExporter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MdExport.Exporter
+{
+    public class BoldExporter : Exporter
+    {
+        public override string ExportHtml(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MarkdownExporter/MarkdownExporterTests/BoldExporterShould.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using MdExport.Exporter;
+using Xunit;
+
+namespace MdExportTests
+{
+    public class BoldExporterShould
+    {
+        private BoldExporter _boldExporter;
+
+        [Fact]
+        public void TransformMarkdownBoldIntoHtmlBold()
+        {
+            var testText = "**This is bold**";
+            var resultText = _boldExporter.ExportHtml(testText);
+            var expectedText = "<b>This is bold</b>";
+            expectedText.Should().BeEquivalentTo(resultText);
+        }
+    }
+}
